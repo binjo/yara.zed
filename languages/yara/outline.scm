2a289f157971f2c4ec222c884455bf2bb0f48cb2
Genwei Jiang <binjo.cn@gmail.com>
2026-06-29 15:26:39 +0800

    feat: enhance document outline with rule sections, metadata, and variables

--- languages/yara/outline.scm
+++ languages/yara/outline.scm
@@ -1,2 +1,16 @@
 (rule_definition
   name: (identifier) @name) @item
+
+(meta_section
+  "meta" @name) @item
+
+(meta_definition
+  key: (identifier) @name) @item
+
+(strings_section
+  "strings" @name) @item
+
+(string_identifier) @name @item
+
+(condition_section
+  "condition" @name) @item
